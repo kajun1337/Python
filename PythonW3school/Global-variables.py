fd12fa66df88fd0411053f147b0c7be65b38fe14
kajun1337 <morbidargels@hotmail.com>
2024-04-16 21:32:40 +0300

    kajun1337

---
+++ PythonW3school/Global-variables.py
@@ -0,0 +1,16 @@
+x = "hakan"
+y= "killa hakan"
+
+def myfunc():
+    print("Git krali" + x)
+
+myfunc()
+
+
+def lostFunc():
+  global x
+  x = "fantastic"
+
+lostFunc())
+
+print("Python is " + x)
---
+++ PythonW3school/ifexample1.py
@@ -0,0 +1,27 @@
+import time 
+
+if 3 > 7:
+    print("C")
+else:
+    print("F")
+
+x = 5
+z =float(7)
+y = "Hello, World"
+
+#integer test
+print(x)
+
+time.sleep(3)
+
+print(x,y,z)
+
+
+
+print(type(z))
+
+"""
+
+test
+
+"""
