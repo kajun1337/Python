a28f19bf25648aadf0203ec5aa0abc098d8083fb
kajun1337 <morbidargels@hotmail.com>
2024-08-31 16:26:10 +0300

    kajun1337

---
+++ istihza/NOTLAR.py
@@ -0,0 +1,33 @@
+import keyword
+print(keyword.kwlist)
+print("\n")
+mehmet = len(keyword.kwlist)
+print(mehmet)
+
+
+gidis_ucreti= 1.5
+gelis_ucreti= 1.4
+
+
+masraf = 22 * (gidis_ucreti + gelis_ucreti)
+print(masraf)
+print("\n")
+print("\n")
+print("\n")
+
+cap = 16
+print(cap / 2)
+
+pow(12,2) # kare hesaplama
+
+
+pow(16, 2, 2) #16 sayısının 2’nci kuvvetini hesapla ve çıkan sayıyı 2’ye bölüp, bölme işleminden kalan sayıyı göster!
+
+
+a = b = 4 #
+
+# _ komutu son girilen öğeyi hafızasında tutuyor.
+
+
+
+
